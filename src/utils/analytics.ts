1c06a960c9e6eda9822a8cf8451893c9879d94e9
LebyakMarki <lebyak.m@gmail.com>
2024-02-19 12:24:56 +0100

    Started project

---
+++ src/utils/analytics.ts
@@ -0,0 +1,65 @@
+import { redis } from "@/lib/redis"
+import {getDate} from "@/utils"
+import { parse } from "date-fns"
+
+type AnalyticsArgs = {
+    retention?: number
+}
+
+type TrackOptions = {
+    persist?: boolean
+}
+
+
+export class Analytics {
+    private retention: number = 60 * 60 * 24 * 7
+
+    constructor(opts?: AnalyticsArgs) {
+        if (opts?.retention) this.retention = opts.retention
+    }
+
+    async track(namespace: string, event: object = {}, opts?: TrackOptions) {
+        // db call to persist this event
+        let key = `analytics::${namespace}`
+        if (!opts?.persist) {
+            key += `::${getDate()}`
+        }
+        await redis.hincrby(key, JSON.stringify(event), 1)
+        if (!opts?.persist) await redis.expire(key, this.retention)
+    }
+
+    async retrieveDays(namespace: string, nDays: number) {
+        type AnalytcisPromise = ReturnType<typeof analytics.retrieve>
+        const promises:AnalytcisPromise[] = []
+
+        for (let index = 0; index < nDays; index++) {
+            const formattedDate = getDate(index);     
+            const promise = analytics.retrieve(namespace, formattedDate)   
+            promises.push(promise)
+        }
+        const fetced = await Promise.all(promises)
+
+        const data = fetced.sort((a,b) => {
+            if (parse(a.date, "dd/MM/yyyy", new Date()) > parse(b.date, "dd/MM/yyyy", new Date())) {
+                return 1
+            } else {
+                return -1
+            }
+        })
+        return data
+    }
+
+    async retrieve(namespace: string, date: string) {
+        const res = await redis.hgetall<Record<string, string>>(`analytics::${namespace}::${date}`)
+        return {
+            date, 
+            events: Object.entries(res ?? []).map(([key, value]) => ({
+                [key]: Number(value)
+            }))
+        }
+    }
+
+
+}
+
+export const analytics = new Analytics()
