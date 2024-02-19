6d336b06934d7c93ae507c8c0f7195840fe92c30
LebyakMarki <lebyak.m@gmail.com>
2024-02-19 23:27:24 +0100

    added two tracking objects

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,12 +1,32 @@
 import AnalyticsDashboard from "@/components/AnalyticsDashboard";
 import { analytics } from "@/utils/analytics";
+import { getDate } from "@/utils";
 
 const Page = async () => {
-    const pageview = await analytics.retrieveDays("pageview", 2)
+    const TRACKING_DAYS = 7
+    const pageviews = await analytics.retrieveDays("pageview", TRACKING_DAYS)
+
+    const totalPageViews = pageviews.reduce((acc, curr) => {
+        return (
+            acc + curr.events.reduce((acc, curr) => {
+                return acc + Object.values(curr)[0]!
+            }, 0)
+        )
+    }, 0)
+
+    const amntVisitorsToday = pageviews.filter((ev) => ev.date === getDate()).reduce((acc, curr) => {
+        return (
+            acc + curr.events.reduce((acc, curr) => {
+                return acc + Object.values(curr)[0]!
+            }, 0)
+        )
+    }, 0)
+
+    const avgVisitorsPerDay = (totalPageViews / TRACKING_DAYS).toFixed(1)
 
     return <div className="min-h-screen w-full py-12 flex justify-center items-center">
         <div className="relative w-full max-w-6xl mx-auto text-white">
-            <AnalyticsDashboard />
+            <AnalyticsDashboard avgVisitorsPerDay={avgVisitorsPerDay} amntVisitorsToday={amntVisitorsToday} />
         </div>
     </div>
 
--- src/components/AnalyticsDashboard.tsx
+++ src/components/AnalyticsDashboard.tsx
@@ -2,12 +2,29 @@
 
 import { Card } from "@tremor/react";
 
-const AnalyticsDashboard = () => {
+interface AnalyticsDashboardProps {
+    avgVisitorsPerDay: string, 
+    amntVisitorsToday: number
+}
+
+
+const AnalyticsDashboard = ({avgVisitorsPerDay, amntVisitorsToday}:AnalyticsDashboardProps) => {
     return <div className="flex flex-col gap-6">
         <div className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6">
             <Card className="w-full mx-auto max-w-xs">
                 <p className="text-tremor-default text-dark-tremor-content">
                     Avg. visitors/day
+                </p>
+                <p className="text-3xl text-dark-tremor-content-strong font-semibold">
+                    {avgVisitorsPerDay}
+                </p>
+            </Card>
+            <Card className="w-full mx-auto max-w-xs">
+                <p className="text-tremor-default text-dark-tremor-content">
+                    Visitors today
+                </p>
+                <p className="text-3xl text-dark-tremor-content-strong font-semibold">
+                    {amntVisitorsToday}
                 </p>
             </Card>
         </div>
